c9e648dd4ea13faf126483a4422b820da2eda834
Caleb <sturg103@mail.chapman.edu>
2022-03-14 20:55:39 -0400

    Update deck_tests.py

--- tests/deck_tests.py
+++ tests/deck_tests.py
@@ -1,21 +1,24 @@
 import unittest
-import lib.concentration
+from concentration import cards
 
 class TestStringMethods(unittest.TestCase):
 
     def (self):
         self.assertEqual('foo'.upper(), 'FOO')
 
-    def test_isupper(self):
+    def full_deck(self):
         self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
 
-    def test_split(self):
+    def empty_deck(self):
         s = 'hello world'
         self.assertEqual(s.split(), ['hello', 'world'])
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
+
+    def random_deck(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
